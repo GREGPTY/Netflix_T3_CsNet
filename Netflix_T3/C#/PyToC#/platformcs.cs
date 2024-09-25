a841272722b9a85117aea392ce47f3c3e8a1d3fc
GREGPTY <31409116+GREGPTY@users.noreply.github.com>
2024-09-25 13:53:07 -0500

    Cosas a realizar de Python

--- Netflix_T3/C#/PyToC#/platformcs.cs
+++ Netflix_T3/C#/PyToC#/platformcs.cs
@@ -5,15 +5,22 @@ using System.Web;
 
 namespace Netflix_T3.C_.PyToC_
 {
+    //Cosas para replicar de python
     //En python El módulo platform permite acceder a los datos de la
     //plataforma subyacente, es decir, hardware, sistema operativo e información sobre la versión del intérprete.
     /*Platform Module contain
      * platform, machine, processor, system, Version*
      Trying repository 2
     Hola T3, official
+    Junto con el modulo de sys que extrae la ruta del archivo y todo lo que este contiene
+    import sys
+    for p in sys.path:
+     print(p)
      */
     public class platformcs
     {
+
+
 
     }
 }
